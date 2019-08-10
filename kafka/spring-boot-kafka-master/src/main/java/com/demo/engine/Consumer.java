8a8f18b2b2cb3b291fbda096a653fd113bedbd2d
amit <amitkmrghosh@gmail.com>
2019-08-10 19:35:50 +0530

    kafka initial commit

---
+++ kafka/spring-boot-kafka-master/src/main/java/com/demo/engine/Consumer.java
@@ -0,0 +1,19 @@
+package com.demo.engine;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Consumer {
+
+    private final Logger logger = LoggerFactory.getLogger(Consumer.class);
+
+    @KafkaListener(topics = "users", groupId = "group_id")
+    public void consume(String message) throws IOException {
+        logger.info(String.format("#### -> Consumed message -> %s", message));
+    }
+}
